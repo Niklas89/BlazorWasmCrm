7f53701bfc35e4549e36512ae210c42529873b30
Niklas <niklas_edelstam@hotmail.com>
2025-03-17 23:04:11 +0100

    created project crm with syncfusion components

---
+++ BlazorWasmCrm/BlazorWasmCrm/Client/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorWasmCrm.Client;
+using Syncfusion.Blazor;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.Services.AddSyncfusionBlazor();
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("BlazorWasmCrm.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorWasmCrm.ServerAPI"));
+
+// generate a new licence key in syncfusion account
+await builder.Build().RunAsync();
